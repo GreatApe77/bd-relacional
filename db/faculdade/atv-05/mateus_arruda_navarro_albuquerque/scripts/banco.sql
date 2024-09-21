7396329a710a4782829f2eb452a14881786f8e49
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-09-21 12:17:45 -0300

    termina script

--- db/faculdade/atv-05/mateus_arruda_navarro_albuquerque/scripts/banco.sql
+++ db/faculdade/atv-05/mateus_arruda_navarro_albuquerque/scripts/banco.sql
@@ -95,3 +95,65 @@ REFERENCES Cliente(idCliente);
 
 ALTER TABLE Endereco ADD CONSTRAINT FOREIGN KEY fk_Endereco_Cliente(Cliente_idCliente)
 REFERENCES Cliente(idCliente);
+
+###INSERÇÕES
+START TRANSACTION;
+INSERT INTO TipoCliente
+(Descricao) 
+VALUES
+("Regular"),
+("Assinante");
+
+
+INSERT INTO Sexo
+(Descricao) 
+VALUES
+("Masculino"),
+("Feminino"),
+("Outro")
+;
+
+INSERT INTO PorteDoPet(
+Descricao)
+VALUES
+("Pequeno"),
+("Médio"),
+("Grande");
+
+INSERT INTO Cliente (Nascimento, Nome, TipoCliente_idTipoCliente, Sexo_idSexo) 
+VALUES ("2000-01-01", "Mateus", 1, 1),
+("1980-12-13", "Jonas", 2, 1),
+("1950-03-27", "Maria", 1, 2);
+
+INSERT INTO Raca (Descricao, Nome, PorteDoPet_idPorteDoPet) 
+VALUES
+ ("Raca chinesa", "Shi-Tzu", 1),
+("Cão de guarda", "Pastor Alemão", 3),
+ ("Cachorro Agressivo", "Pitbull", 2),
+ ("Cachorro Alemao", "Weimaraner", 3)
+ ;
+ INSERT INTO Pet(Nascimento, Nome, Cliente_idCliente, Sexo_idSexo, Raca_idRaca)
+VALUES
+("2014-10-10","Nina",1,2,1),
+("2009-10-10","Safira",1,2,4),
+("2010-10-10","Barbas",3,1,2),
+("2020-10-1","Flor",3,2,3);
+
+INSERT INTO Telefone (Numero, Cliente_idCliente)
+VALUES ('81828384', 1),
+('81828385', 1),
+('11223344', 2),
+('32123454', 3),
+('98877665', 3);
+
+INSERT INTO Endereco 
+(Logradouro, Numero, Complemento, Bairro, Cidade, CEP, UF, Cliente_idCliente)
+VALUES 
+("Rua A", "123", "Apto 12", "Centro", "Fortaleza", "12345678", "CE", 1),
+("Rua B", "456", "Apto 13", "Aldeota", "Fortaleza", "60192192", "CE", 1),
+("Rua C", "789", "Apto 14", "Meireles", "Fortaleza", "60192193", "CE", 2),
+("Rua D", "1011", "Apto 15", "Messejana", "Fortaleza", "60192194", "CE", 2),
+("Rua E", "1213", "Casa 1", "Aerolândia", "Fortaleza", "50192195", "CE", 2),
+("Rua M", "1415", "Apto 1502", "Bom Jardim", "Fortaleza", "60192099", "CE", 3);
+COMMIT;
+SELECT * FROM Cliente;
